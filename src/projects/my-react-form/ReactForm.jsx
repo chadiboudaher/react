96b563af855e4139d51b2d777659717916c2bf08
chadiboudaher <chadiboudaher7@gmail.com>
2026-04-06 16:13:24 +0300

    React form project : Code the logic for the form in react

--- src/projects/my-react-form/ReactForm.jsx
+++ src/projects/my-react-form/ReactForm.jsx
@@ -1,32 +1,151 @@
 import "./ReactForm.css";
-// import { React, useState } from "react";
+import React, { useState } from "react";
 
 function ReactForm() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [contact, setContact] = useState("");
+  const [gender, setGender] = useState("male");
+  const [subjects, setSubjects] = useState({
+    english: true,
+    maths: false,
+    physics: false,
+  });
+  const [resume, setResume] = useState("");
+  const [url, setUrl] = useState("");
+  const [selectedOption, setSelectedOption] = useState("");
+  const [about, setAbout] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(
+      firstName,
+      lastName,
+      email,
+      contact,
+      gender,
+      selectedOption,
+      subjects,
+      resume,
+      url,
+      about,
+    );
+  };
+
+  const handleSubjectChange = (sub) => {
+    setSubjects((prev) => ({
+      ...prev,
+      [sub]: !prev[sub],
+    }));
+  };
+
+  const handleReset = () => {
+    // Reset all state variables here
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setContact("");
+    setGender("male");
+    setSubjects({
+      english: true,
+      maths: false,
+      physics: false,
+    });
+    setResume("");
+    setUrl("");
+    setSelectedOption("");
+    setAbout("");
+  };
   return (
     <>
       <div className="app">
         <h1>Form in React</h1>
         <fieldset>
-          <form action="" method="get">
+          <form onSubmit={handleSubmit}>
             <label htmlFor="firstname">First Name*</label>
-            <input type="text" name="firstname" id="firstname" />
+            <input
+              type="text"
+              name="firstname"
+              id="firstname"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              placeholder="Enter First Name"
+              required
+            />
             <label htmlFor="lastname">Last Name*</label>
-            <input type="text" name="lastname" id="lastname" />
-            <label for="email">Enter Email* </label>
-            <input type="email" name="email" id="email" />
-            <label for="tel">Contact*</label>
-            <input type="tel" name="contact" id="contact" />
+            <input
+              type="text"
+              name="lastname"
+              id="lastname"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              placeholder="Enter Last Name"
+              required
+            />
+            <label htmlFor="email">Enter Email* </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Enter email"
+              required
+            />
+            <label htmlFor="tel">Contact*</label>
+            <input
+              type="tel"
+              name="contact"
+              id="contact"
+              value={contact}
+              onChange={(e) => setContact(e.target.value)}
+              placeholder="Enter Mobile number"
+              required
+            />
             <label htmlFor="gender">Gender*</label>
-            <input type="radio" name="gender" id="male" value="male" />
+            <input
+              type="radio"
+              name="gender"
+              id="male"
+              value="male"
+              checked={gender === "male"}
+              onChange={(e) => setGender(e.target.value)}
+            />
             Male
-            <input type="radio" name="gender" id="female" value="female" />
+            <input
+              type="radio"
+              name="gender"
+              id="female"
+              value="female"
+              checked={gender === "female"}
+              onChange={(e) => setGender(e.target.value)}
+            />
             Female
             <label htmlFor="lang">Your Best Subject</label>
-            <input type="checkbox" name="lang" id="english" />
+            <input
+              type="checkbox"
+              name="lang"
+              id="english"
+              checked={subjects.english === true}
+              onChange={(e) => handleSubjectChange("english")}
+            />
             English
-            <input type="checkbox" name="lang" id="maths" />
+            <input
+              type="checkbox"
+              name="lang"
+              id="maths"
+              checked={subjects.maths === true}
+              onChange={(e) => handleSubjectChange("maths")}
+            />
             Maths
-            <input type="checkbox" name="lang" id="physics" />
+            <input
+              type="checkbox"
+              name="lang"
+              id="physics"
+              checked={subjects.physics === true}
+              onChange={(e) => handleSubjectChange("physics")}
+            />
             Physics
             <label htmlFor="file">Upload Resume*</label>
             <input
@@ -34,14 +153,33 @@ function ReactForm() {
               name="file"
               id="file"
               placeholder="Enter Upload File"
+              onChange={(e) => setResume(e.target.files[0])}
+              required
             />
             <label htmlFor="url">Enter URL*</label>
-            <input type="url" name="url" id="url" placeholder="Enter url" />
+            <input
+              type="url"
+              name="url"
+              id="url"
+              onChange={(e) => setUrl(e.target.value)}
+              placeholder="Enter url"
+              required
+            />
             <label>Select your Choice</label>
-            <select name="select" id="select">
+            <select
+              name="select"
+              id="select"
+              value={selectedOption}
+              onChange={(e) => setSelectedOption(e.target.value)}
+            >
               <option value="" disabled>
                 Select your Ans
               </option>
+              <optgroup label="Beginers">
+                <option value="1">HTML</option>
+                <option value="2">CSS</option>
+                <option value="3">JavaScript</option>
+              </optgroup>
               <optgroup label="advance">
                 <option value="4">React</option>
                 <option value="5">Node</option>
@@ -55,9 +193,12 @@ function ReactForm() {
               id="about"
               cols="30"
               rows="10"
+              value={about}
+              onChange={(e) => setAbout(e.target.value)}
               placeholder="About your self"
+              required
             ></textarea>
-            <button type="reset" value="reset">
+            <button type="reset" value="reset" onClick={() => handleReset()}>
               Reset
             </button>
             <button type="submit" value="submit">
